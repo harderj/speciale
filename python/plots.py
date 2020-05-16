d51befb19f9b33cd7612a9927ad70500c1b5c650
Jacob Harder <harderjacob@gmail.com>
2020-05-16 14:48:47 +0200

    gridworld done, starting on cartpole

--- python/plots.py
+++ python/plots.py
@@ -63,15 +63,53 @@ def plotValueGrid(ax, m, bound=25):
         ax.set_xticks([])
     ax.set_yticks([])
 
-vgrids = [m1rPol, m400rPol, m3, m400]
-fig, axs = plt.subplots(1,len(vgrids))
-for m in range(len(vgrids)):
-    plotValueGrid(axs[m], vgrids[m])
-axs[0].set_xlabel('$V_{1, \\tau_r}$')
-axs[1].set_xlabel('$V_{400, \\tau_r}$')
-axs[2].set_xlabel('$V_{3}^*$')
-axs[3].set_xlabel('$V_{400}^*$')
-plt.show()
+def fourGrids():
+    vgrids = [m1rPol, m400rPol, m3, m400]
+    fig, axs = plt.subplots(1,len(vgrids))
+    for m in range(len(vgrids)):
+        plotValueGrid(axs[m], vgrids[m])
+    axs[0].set_xlabel('$V_{1, \\tau_r}$')
+    axs[1].set_xlabel('$V_{400, \\tau_r}$')
+    axs[2].set_xlabel('$V_{3}^*$')
+    axs[3].set_xlabel('$V_{400}^*$')
+    plt.show()
+
+def convGraph():
+    n = 100
+    xs = np.array(range(n))
+    voptmax = max(v400) # |V*|
+    vtauroptmax = max(v400rPol) # |V_taur|
+    gys = gamma**xs * 100 # general theoretical upper bound
+    vys = gamma**xs * voptmax # theoretical upper bound for V^*
+    tys = gamma**xs * vtauroptmax # theoretical upper bound for V_taur
+    ays = np.zeros(n) # |V*k - V*|
+    rys = np.zeros(n) # |V_{taur,k} - V_taur|
+    v = np.zeros(nstates)
+    vr = np.zeros(nstates)
+    for i in range(n):
+        v = tBell(v)
+        vr = tPol(rPol, vr)
+        ays[i] = max(abs(v - v400))
+        rys[i] = max(abs(vr - v400rPol))
+    fig, ax = plt.subplots()
+    blue1 = '#1f77b4' # 'category10 blue' (standard f. pyplot, Tableau, a.o.)
+    orange1 = '#ff7f0e' # 'category10 orange'
+    ax.plot(xs, ays, color = blue1)
+    ax.plot(xs, rys, color = orange1)
+    ax.plot(xs, vys, ls=':', color = blue1)
+    ax.plot(xs, tys, ls=':', color = orange1)
+    ax.plot(xs, gys, ls='--', color='black')
+    ax.set_yscale('log')
+    ax.legend(['$|V_k^* - V_{400}^*|$',
+        '$|V_{\\tau_r, k} - V_{\\tau_r, 400}|$',
+        '$\gamma^k |V_{400}^*|$',
+        '$\gamma^k |V_{\\tau_r, 400}|$',
+        '$\gamma^k V_{\max} / (1-\gamma)$'])
+    ax.set_xlabel('$k$')
+    ax.set_ylabel('value (expected acc. rewards)')
+    plt.show()
+
+convGraph()
 
 
 
